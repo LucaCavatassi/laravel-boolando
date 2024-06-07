1bfe46ce28bc9716827373922efbbc5ad5d8330d
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-06-07 17:39:26 +0200

    feat:footer links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,26 @@
 <footer>
     <div class="footer-cont d-flex align-items-center justify-content-between">
+        {{-- LEGAL --}}
         <div>
             <h5 class="m-0">Boolando s.r.l.</h5>
-            <ul class="px-0 d-flex gap-3">
+            <ul class="px-0 d-flex gap-3 m-0">
                 <li><a href="">Informazioni Legali</a></li>
                 <li><a href="">Informativa sulla privacy</a></li>
                 <li><a href="">Diritto di recesso</a></li>
             </ul>
         </div>
+        {{-- LEGAL --}}
+
+        {{-- SOCIAL --}}
+        <div>
+            <h5 class="m-0">Trovaci anche su</h5>
+            <ul class="px-0 d-flex gap-3 m-0">
+                <li><a href=""><i class="fa-brands fa-square-twitter"></i></a></li>
+                <li><a href=""><i class="fa-brands fa-square-facebook"></i></a></li>
+                <li><a href=""><i class="fa-brands fa-square-instagram"></i></a></li>
+                <li><a href=""><i class="fa-brands fa-square-whatsapp"></i></a></li>
+            </ul>
+        </div>
+        {{-- SOCIAL --}}
     </div>
 </footer>
