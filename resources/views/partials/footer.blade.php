cc5afbb8919e8436c8c20b43b4010f65e40126a0
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-06-07 17:34:36 +0200

    feat: footer legal links added

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <h1>FOOTER</h1>
-</div>
+<footer>
+    <div class="footer-cont d-flex align-items-center justify-content-between">
+        <div>
+            <h5 class="m-0">Boolando s.r.l.</h5>
+            <ul class="px-0 d-flex gap-3">
+                <li><a href="">Informazioni Legali</a></li>
+                <li><a href="">Informativa sulla privacy</a></li>
+                <li><a href="">Diritto di recesso</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
