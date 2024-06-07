fd3297f0b43727b7ffe19602eb904a2b1690c87e
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-06-07 17:06:01 +0200

    feat: like-badge

--- resources/views/partials/product-card.blade.php
+++ resources/views/partials/product-card.blade.php
@@ -4,12 +4,19 @@
     <div class="images">
         <img class="imgfront" src={{ asset("img/".$product["frontImage"]) }} alt="Levis Relaxed Fit Tee">
         <img class="imgback" src={{ asset("img/".$product["backImage"]) }} alt="Levis Relaxed Fit Tee">
+
+        {{-- LIKE BADGE --}}
+        <div class="like-badge">
+            <span class="unliked"><i class="fa-regular fa-heart"></i></span>
+            <span class="liked"><i class="fa-solid fa-heart"></i></span>
+        </div>
+        {{-- LIKE BADGE --}}
     </div>
     <!-- IMAGES -->
 
     {{-- ITEM INFO --}}
     <div class="py-2">
-        <a class="fs-5" href="">{{ $product["brand"] }}</a>
+        <a class="fs-6" href="">{{ $product["brand"] }}</a>
         <h3 class="m-0">{{ $product["name"] }}</h3>
         <span class="newprice">{{$product["price"]}}€</span>
     </div>
