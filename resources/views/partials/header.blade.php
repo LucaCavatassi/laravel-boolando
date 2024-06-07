b106693a4709297c37291e2faa4b3c2e4e11cfdc
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-06-07 15:47:51 +0200

    feat: nav gender on header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <h1>HEADER</h1>
-</div>
+<header>
+    <div class="d-flex  align-items-center justify-content-between">
+        <ul class="d-flex align-items-center gap-3 fs-4 text-white fw-bolder">
+            <li><a href="">Donna</a></li>
+            <li><a href="">Uomo</a></li>
+            <li><a href="">Bambino</a></li>
+        </ul>
+    </div>
+</header>
